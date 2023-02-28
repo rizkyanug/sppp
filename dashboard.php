920c740383c6ef680cc2f53a340cdde901425daf
Rizky <suierghusie@gmail.com>
2023-02-28 10:25:22 +0700

    initial commit

---
+++ dashboard.php
@@ -0,0 +1,167 @@
+<?php
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  include ('tampilan/header.php');
+  include ('tampilan/sidebar.php');
+  include ('tampilan/footer.php');
+?>
+ <!-- Main Content -->
+      <div class="main-content bg-primary">
+        <section class="section">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-stats">
+                  <div class="card-stats-title">DATA KELAS -
+                    <div class="dropdown d-inline">
+                    <a data-toggle="dropdown" href="#" id="orders-month">XII OTKP 1</a>
+                    </div>
+                  </div>
+                  <div class="card-stats-items">
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">33</div>
+                      <div class="card-stats-item-label">Siswa</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">12</div>
+                      <div class="card-stats-item-label">B Lunas</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">21</div>
+                      <div class="card-stats-item-label">Lunas</div>
+                    </div>
+                  </div>
+                  
+                </div>
+                <div class="card-icon shadow-info bg-primary">
+                  <i class="fas fa-users"></i>
+                
+        </section>
+
+        <section class="section">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-stats">
+                  <div class="card-stats-title">DATA KELAS -
+                    <div class="dropdown d-inline">
+                      <a data-toggle="dropdown" href="#" id="orders-month">XII OTKP 2</a>
+                    </div>
+                  </div>
+                  <div class="card-stats-items">
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">33</div>
+                      <div class="card-stats-item-label">Siswa</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">12</div>
+                      <div class="card-stats-item-label">B Lunas</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">21</div>
+                      <div class="card-stats-item-label">Lunas</div>
+                    </div>
+                  </div>
+                  
+                </div>
+                <div class="card-icon shadow-info bg-primary">
+                  <i class="fas fa-users"></i>
+                
+        </section>
+
+        <section class="section">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-stats">
+                  <div class="card-stats-title">DATA KELAS -
+                    <div class="dropdown d-inline">
+                      <a data-toggle="dropdown" href="#" id="orders-month">XII AKL</a>
+                    </div>
+                  </div>
+                  <div class="card-stats-items">
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">33</div>
+                      <div class="card-stats-item-label">Siswa</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">12</div>
+                      <div class="card-stats-item-label">B Lunas</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">21</div>
+                      <div class="card-stats-item-label">Lunas</div>
+                    </div>
+                  </div>
+                  
+                </div>
+                <div class="card-icon shadow-info bg-primary">
+                  <i class="fas fa-users"></i>
+                
+        </section>
+
+        <section class="section">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-stats">
+                  <div class="card-stats-title">DATA KELAS -
+                    <div class="dropdown d-inline">
+                      <a data-toggle="dropdown" href="#" id="orders-month">XII BDP</a>
+                    </div>
+                  </div>
+                  <div class="card-stats-items">
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">33</div>
+                      <div class="card-stats-item-label">Siswa</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">12</div>
+                      <div class="card-stats-item-label">B Lunas</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">21</div>
+                      <div class="card-stats-item-label">Lunas</div>
+                    </div>
+                  </div>
+                  
+                </div>
+                <div class="card-icon shadow-info bg-primary">
+                  <i class="fas fa-users"></i>
+                
+        </section>
+
+        <section class="section">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-stats">
+                  <div class="card-stats-title">DATA KELAS -
+                    <div class="dropdown d-inline">
+                      <a data-toggle="dropdown" href="#" id="orders-month">XII RPL</a>
+                    </div>
+                  </div>
+                  <div class="card-stats-items">
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">24</div>
+                      <div class="card-stats-item-label">Siswa</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">24</div>
+                      <div class="card-stats-item-label">B Lunas</div>
+                    </div>
+                    <div class="card-stats-item">
+                      <div class="card-stats-item-count">0</div>
+                      <div class="card-stats-item-label">Lunas</div>
+                    </div>
+                  </div>
+                  
+                </div>
+                <div class="card-icon shadow-info bg-primary">
+                  <i class="fas fa-users"></i>
+                
+        </section>
+
+      </div>
+    </div>
+  </div>
+</body>
